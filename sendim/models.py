3fcecea9b2317b98d58d43688602f94545977cff
Anthony MONTHE <montheanthony@hotmail.com>
2012-09-05 20:57:40 +0200

    Add method getLastAlert() in Event

--- sendim/models.py
+++ sendim/models.py
@@ -1,27 +1,35 @@
-from referentiel.models import Host, Status, Service
+from django.db.models import Q
 from django.db import models
 
+from referentiel.models import Host, Status, Service
+
 class Event(models.Model) :
-	element = models.ForeignKey(Host)
-	date = models.DateTimeField()
-	criticity = models.CharField(max_length=30)
-	message = models.CharField(max_length=300)
-	glpi = models.IntegerField(blank=True, null=True)
-	mail = models.BooleanField(default=False)
+    element = models.ForeignKey(Host)
+    date = models.DateTimeField()
+    criticity = models.CharField(max_length=30)
+    message = models.CharField(max_length=300)
+    glpi = models.IntegerField(blank=True, null=True)
+    mail = models.BooleanField(default=False)
 
-	def __unicode__(self) :
-		return self.element.host+': '+self.message
+    def __unicode__(self) :
+        return self.element.host+': '+self.message
 
-	def openTicket(self) :
-		pass
+    def getLastAlert(self, isUp=False):
+        return Alert.objects.filter(
+            Q(event__pk=self.pk),
+            Q(status__status__exact='OK') | Q(status__status__exact='UP')
+        ).order_by('-pk')[0]
+
+    def openTicket(self) :
+    	pass
 
 class Alert(models.Model) :
-	host = models.ForeignKey(Host)
-	service = models.ForeignKey(Service)
-	status = models.ForeignKey(Status)
-	date = models.DateTimeField()
-	info = models.CharField(max_length=300)
-	event = models.ForeignKey(Event, blank=True, null=True)
+    host = models.ForeignKey(Host)
+    service = models.ForeignKey(Service)
+    status = models.ForeignKey(Status)
+    date = models.DateTimeField()
+    info = models.CharField(max_length=300)
+    event = models.ForeignKey(Event, blank=True, null=True)
 
-	def __unicode__(self) :
-		return self.host.host+':'+self.service.service
+    def __unicode__(self) :
+        return self.host.host+':'+self.service.service
