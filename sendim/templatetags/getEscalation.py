8b3aecbb6ca11017c87efb9a754584c40beb64a7
Anthony MONTHE <montheanthony@hotmail.com>
2012-09-23 05:17:56 +0200

    Fix again

--- sendim/templatetags/getEscalation.py
+++ sendim/templatetags/getEscalation.py
@@ -9,7 +9,7 @@ register = template.Library()
 
 @register.filter(name='getEscalation')
 def getEscalation(value, arg=None) :
-    As = Alert.objects.order_by('-pk').filter(event__pk__exact=value).exclude(status__status__exact='OK')
+    As = Alert.objects.order_by('-pk').filter(event__pk__exact=value).exclude(Q(status__status__exact='OK') | Q(status__status__exact='UP'))
     if not As : return None
 
     R = getReference(A)
