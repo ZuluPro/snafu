9f42c9cb1527d82d1d919a8db734b01f7b98ee3d
Anthony MONTHE <montheanthony@hotmail.com>
2012-09-23 03:06:13 +0200

    Fix in ref/defs.py

--- referentiel/defs.py
+++ referentiel/defs.py
@@ -1,4 +1,5 @@
 from referentiel.models import Reference, Traduction, Host, Service, Status
+from common import logprint
 
 def getReference(A, byHost=True, byService=True, byStatus=True ) :
     Rs = Reference.objects.all()
@@ -6,7 +7,7 @@ def getReference(A, byHost=True, byService=True, byStatus=True ) :
     if byService : Rs = Rs.filter(service=A.service)
     if byStatus : Rs = Rs.filter(status=A.status)
 
-    if not Rs : R = None ; logprint('No Reference for Alert #'+str(self.pk), 'red')
+    if not Rs : R = None ; logprint('No Reference for Alert #'+str(A.pk), 'red')
     else : R = Rs[0]
 
     return R
