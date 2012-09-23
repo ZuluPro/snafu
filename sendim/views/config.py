05ee26cacb74316144b2afd00a36b70335317e9f
Anthony MONTHE <montheanthony@hotmail.com>
2012-09-23 15:03:47 +0200

    Create POST for Ref

--- sendim/views/config.py
+++ sendim/views/config.py
@@ -22,9 +22,43 @@ def configuration(request) :
                     T.save()
                     logprint("Save Traduction #"+str(T.pk), "green")
             if 'apply' in request.POST :
-                print 123213
                 for A in Alert.objects.filter(service=service,traduction=None) :
                     A.linkToTraduction()
+                    
+
+        elif request.POST['action'] == 'addRef' :
+            host = Host.objects.get(pk=request.POST['host'])
+            service = Service.objects.get(pk=request.POST['service'])
+            for status in ('WARNING','CRITICAL','UNKNOWN') :
+                R = Reference(
+                    host = host,
+                    service = service,
+                    status = Status.objects.get(status=status.lower()),
+
+                    escalation_contact = request.POST['escalation_contact'],
+                    tendancy = request.POST['tendancy'],
+                    outage = request.POST['outage'],
+                    explanation = request.POST['explanation'],
+                    origin = request.POST['origin'],
+                    procedure = request.POST['procedure'],
+                
+                    mail_type = MailType.objects.get(pk=request.POST['mail_type']),
+                    mail_group = MailGroup.objects.get(pk=request.POST['mail_group']),
+                
+                    glpi_category = GlpiCategory.objects.get(pk=request.POST['glpi_category']),
+                    glpi_source = request.POST['glpi_source'],
+                    glpi_dst_group = GlpiGroup.objects.get(pk=request.POST['glpi_dst_group']),
+                    glpi_supplier = GlpiSupplier.objects.get(pk=request.POST['glpi_supplier']),
+                )
+                R.mail_criticity = MailCriticity.objects.get(pk=request.POST[status.lower()+'_criticity'])
+                R.glpi_urgency = GlpiUrgency.objects.get(pk=request.POST[status.lower()+'_urgency'])
+                R.glpi_priority = GlpiPriority.objects.get(pk=request.POST[status.lower()+'_priority'])
+                R.glpi_impact = GlpiImpact.objects.get(pk=request.POST[status.lower()+'_impact'])
+                R.save()
+
+                if 'apply' in request.POST :
+                    for A in Alert.objects.filter(host=host, service=service,reference=None) :
+                        A.linkToReference()
 
     return render(request, 'configuration/index.html', {
         'alerts':Alert.objects.all(),
