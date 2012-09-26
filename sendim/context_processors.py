fc95b9d8d00638d32c2f4ea9059cb3e03167e9c0
Anthony MONTHE <montheanthony@hotmail.com>
2012-09-26 17:28:21 +0200

    Mod context

--- sendim/context_processors.py
+++ sendim/context_processors.py
@@ -2,8 +2,15 @@ from django.conf import settings
 
 from sendim.models import Alert
 
-def glpi(request):
+def sendim_context(request):
     return {
+        'nagios': { 
+            'index_url':settings.SNAFU['nagios-url'],
+            'host_status_url':settings.SNAFU['nagios-status']+'?host='
+        },
+        'alert': {
+            'without_ref':Alert.objects.filter(reference=None).count()
+        },
         'glpi': { 
             'index_url':settings.SNAFU['glpi-url'],
             'ticket_url':settings.SNAFU['glpi-url']+'front/ticket.form.php?id=',
@@ -12,23 +19,5 @@ def glpi(request):
             'user_url': settings.SNAFU['glpi-url']+'front/user.form.php?id=',
             'group_url': settings.SNAFU['glpi-url']+'front/group.form.php?id=',
             'itilcategory_url': settings.SNAFU['glpi-url']+'front/itilcategory.form.php?id='
-        }
-    }
-
-def nagios(request):
-    print 123
-    return {
-        'nagios': { 
-            'index_url':settings.SNAFU['nagios-url'],
-            'host_status_url':settings.SNAFU['nagios-status']+'?host='
-        },
-        'ref': {
-            'ref':Alert.objects.filter(reference=None).count()
-        }
-    }
-
-def refe(request):
-    return {
-        'ref': { 
         }
     }
