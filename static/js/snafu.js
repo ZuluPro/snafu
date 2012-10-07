199eb2a2fc1fe798b133b7d2820bb12b41e3c770
Anthony MONTHE <montheanthony@hotmail.com>
2012-10-07 08:42:04 +0200

    $.fn.AgrForm use $.fn.UseModal

--- static/js/snafu.js
+++ static/js/snafu.js
@@ -1,34 +1,32 @@
 $(document).ready(function() {
 
-    $('input[filter]').change( function() {
-            $.get('/events/filter',
-                {
-                    'pk': $('#pk').val(),
-                    'element': $('#element').val(),
-                    'glpi' : $('#glpi').val(),
-                    'message' : $('#message').val(),
-                    'date' : $('#datepicker').val()
-                }, function(data) {
-                $('#E_tbody').html( data );
-            })
-    });
-
-  $.fn.AgrForm = function(){
-    ids = [];
-    $('input:checked').each( function(box) { ids.push( $(this).val() )})
-    $.get('/eventsAgr', { events: ids }, function(data) {
-      $('#infoModal').html( data );
-      $('#infoModal').modal('toggle');
-    });
-    ids.length = 0;
-  }
-
-  $.fn.UseModal = function(url,eventPk){
-  $.get(url,{ eventPk: eventPk }, function(data) {
+  $.fn.UseModal = function(url,GETdict){
+  $.get(url,GETdict, function(data) {
        $('#infoModal').html( data );
        $('#infoModal').modal('toggle')
      });
   }
+
+  $('input[filter]').change( function() {
+    $.get('/events/filter',
+      {
+        'pk': $('#pk').val(),
+        'element': $('#element').val(),
+        'glpi' : $('#glpi').val(),
+        'message' : $('#message').val(),
+        'date' : $('#datepicker').val()
+      }, function(data) {
+    $('#E_tbody').html( data );
+    })
+  });
+
+  $.fn.AgrForm = function(){
+    ids = [];
+    $('input:checked').each( function(box) { ids.push( $(this).val() )})
+    $.fn.UseModal('/eventsAgr', { events: ids } )
+    ids.length = 0;
+  }
+
 
 });
 
