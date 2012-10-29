a68e4de7fca62e4838e4df44061e91986da72fac
Anthony MONTHE <montheanthony@hotmail.com>
2012-10-29 17:10:30 +0100

    Add initial_data.json

--- referentiel/management/commands/populate.py
+++ referentiel/management/commands/populate.py
@@ -1,15 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-
 from django.db.utils import IntegrityError
 
 from referentiel.models import *
 from sendim.models import *
 
-from common import logprint
-import os,sys,re
-import sqlite3
 import xmlrpclib
+from common import logprint
 
 
 serverUrl = settings.SNAFU['glpi-xmlrpc']
@@ -17,9 +14,6 @@ loginData = { 'login_name':settings.SNAFU['glpi-login'], 'login_password':settin
 ws = xmlrpclib.Server(serverUrl, verbose=False, allow_none=True)
 ws_session = ws.glpi.doLogin(loginData)
 
-impact = { 'Bas':2, 'Moyen':3, 'Haut':4, u'Tr\xe8s haut':5 }
-urgency = { 'Basse':2, 'Moyenne':3, 'Haute':4, u'Tr\xe8s haute':5 }
-                                
 class Command(BaseCommand) :
     args = None
     def handle(self, *args, **options) :
@@ -31,8 +25,6 @@ class Command(BaseCommand) :
 			choosen=True
 		).save()
 		logprint('Add first mail template')
-
-	if not Service.objects.filter(service='Host status') : Service(service='Host status').save()
 
         computers = ws.glpi.listObjects( { 'session': ws_session['session'], 'itemtype': 'computer', 'limit': 2000 } )
         for host in computers :
@@ -66,45 +58,3 @@ class Command(BaseCommand) :
                 H.save()	
 		logprint('Add GLPI group : '+GU.glpi_group, 'green')
             except IntegrityError : logprint('Group ' +GU.glpi_group+ ' already exists', 'yellow')
-
-        for status in ['WARNING','CRITICAL','OK', 'DOWN','UP', 'UNKNOWN' ] :
-                try:
-                        S = Status(status=status)
-                        S.save()
-                except IntegrityError : pass
-
-        for urg in urgency :
-                try:
-                        H = GlpiUrgency(glpi_urgency=urg, glpi_id=urgency[urg] )
-                        H.save()
-                except IntegrityError : pass
-
-        for imp in impact :
-                try:
-                        H = GlpiImpact(glpi_impact=imp, glpi_id=impact[imp] )
-                        H.save()
-                except IntegrityError : pass
-                
-        for prio in urgency :
-                try:
-                        H = GlpiPriority(glpi_priority=prio, glpi_id=urgency[prio] )
-                        H.save()
-                except IntegrityError : pass
-
-        for criticity in ['Mineur','Majeur'] :
-                try:
-                        C = MailCriticity(mail_criticity=criticity)
-                        C.save()	
-                except IntegrityError : pass
-
-#        for cat in categories :
-#                try:
-#                        H = GlpiCategory(glpi_category=cat, glpi_id=categories[cat] )
-#                        H.save()	
-#                except : pass
-
-#        for sup in suppliers :
-#                try:
-#                        H = GlpiSupplier(glpi_supplier=sup, glpi_id=suppliers[sup] )
-#                        H.save()	
-#                except : pass
