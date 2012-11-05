59cddbea3e6772567d674270d91fc759f945caa4
Anthony MONTHE <montheanthony@hotmail.com>
2012-11-05 22:29:54 +0100

    Control HostRefForm

--- referentiel/forms.py
+++ referentiel/forms.py
@@ -2,17 +2,25 @@ from django import forms
 from referentiel.models import *
 
 class ReferenceForm(forms.ModelForm):
+    mail_criticity = forms.ModelChoiceField(MailCriticity.objects.all(), required=True, initial=1)
+    glpi_urgency = forms.ModelChoiceField(GlpiUrgency.objects.all(), required=True, initial=1)
+    glpi_priority = forms.ModelChoiceField(GlpiPriority.objects.all(), required=True, initial=1)
+    glpi_impact = forms.ModelChoiceField(GlpiImpact.objects.all(), required=True, initial=1)
+    glpi_source = forms.CharField(initial='Supervision')
+    
     class Meta:
         model = Reference
 
 class HostReferenceForm(ReferenceForm):
     service = forms.ModelChoiceField(
       Service.objects.filter(pk=1),
-      initial=1
+      initial=1,
+      widget=forms.widgets.HiddenInput()
     )
     status = forms.ModelChoiceField(
       Status.objects.filter(name='DOWN'),
-      initial=4
+      initial=4,
+      widget=forms.widgets.HiddenInput()
     )
 
     def __init__(self, *args, **kwargs):
