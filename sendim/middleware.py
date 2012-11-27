430b21d1ea6bcb03531b9094af256dd8f5049d9d
Anthony MONTHE <montheanthony@hotmail.com>
2012-11-27 22:31:25 +0100

    Add middleware fro find missing element in db

--- sendim/middleware.py
+++ sendim/middleware.py
@@ -1,5 +1,9 @@
-from djcelery.models import TaskMeta
 from django.contrib import messages
+
+from djcelery.models import TaskMeta
+
+from sendim.models import Supervisor
+from referentiel.models import MailType,MailGroup
 
 class CeleryMessageMiddleware(object):
 
@@ -14,3 +18,18 @@ class CeleryMessageMiddleware(object):
                     messages.add_message(request, messages.ERROR,'<b>Erreur de lecture de '+task_result[0]+' : '+str(task_result[2])+'</b>')
 
                 task.delete()
+
+class SnafuRequirementMiddleware(object):
+
+    def process_request(self,request):
+
+        if request.user.is_authenticated() and not request.is_ajax():
+            messages_list = list()
+            if not Supervisor.objects.exists() :
+                messages_list.append('<li>Au moins 1 superviseur</li>')
+            if not MailType.objects.exists() :
+                messages_list.append('<li>Au moins 1 type de mail</li>')
+            if not MailGroup.objects.exists() :
+                messages_list.append('<li>Au moins 1 groupe de destinataire mail</li>')
+            if messages_list :
+                messages.add_message(request, messages.WARNING,u"<b>Attention</b>, il manque des donn\xe9es pour fonctionner :<ul>"+''.join(messages_list)+'</ul>')
