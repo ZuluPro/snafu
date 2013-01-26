29a81a09bbcaaff9515858d3a9a97e09e2b78d90
Anthony MONTHE <montheanthony@hotmail.com>
2013-01-26 03:34:32 +0100

    Mod push_alert.sh

--- extras/push_alert.sh
+++ extras/push_alert.sh
@@ -20,17 +20,17 @@ function get_help {
     Option :
     -w | --webservice : Snafu webservice's URL
     -p | --port : Snafu webservice's port (default=80)
-    -h | --host : Nagios host's name ($HOSTNAME$)
-    -s | --service : Nagios service's name ($SERVICEDISPLAYNAME$)
-    -S | --status : Nagios service's status ($SERVICESTATE$)
-    -d | --date : Alert's date ($SHORTDATETIME$)
-    -i | --info : Alert's information ($SERVICEOUTPUT$)
+    -h | --host : Nagios host's name (\$HOSTNAME\$)
+    -s | --service : Nagios service's name (\$SERVICEDISPLAYNAME\$)
+    -S | --status : Nagios service's status (\$SERVICESTATE\$)
+    -d | --date : Alert's date (\$SHORTDATETIME\$)
+    -i | --info : Alert's information (\$SERVICEOUTPUT\$)
     --help : This help message
 ";
 }
 
+# Walk on all arguments
 while (( "$#" )) ; do
-  echo $1 $2
   case $1 in
     -w|--webservice) snafu=$2 ;;
     -p|--port) port=$2 ;;
@@ -52,22 +52,27 @@ while (( "$#" )) ; do
   shift 2
 done
 
+# Check if there's no missing args
 [[ -z "$port" ]] && port=80
+[[ -z "$date" ]] && date=$(date "+%Y-%m-%d %H:%M:%S")
 if [[ -z "$snafu" || -z "$host" || -z "$service" || -z "$status" || -z "$date" || -z "$info" ]] ; then
   echo 'Missing arguments !'
   get_help
   exit 1
 fi
 
+# Make an XML-RPC message
 msg="\n<?xml version='1.0'?>\n<methodCall>\n<methodName>pushAlert</methodName>\n<params>\n<param>\n<value><string>$host</string></value>\n</param>\n<param>\n<value><string>$service</string></value>\n</param>\n<param>\n<value><string>$status</string></value>\n</param>\n<param>\n<value><string>$info</string></value>\n</param>\n<param>\n<value><string>$(uname -n)</string></value>\n</param>\n<param>\n<value><string>$date</string></value>\n</param>\n</params>\n</methodCall>"
 
+# Count message's length for put it in header
 length=$(echo -e $msg|wc -c)
+# Make header, with 'bash script' as HTTP client
+header="POST /snafu/webservice HTTP/1.1\nHost: $snafu\nUser-Agent: bash script\nContent-Type: text/xml\nContent-length: $length\n"
 
-	header="POST /snafu/webservice HTTP/1.1\nHost: $snafu\nUser-Agent: bash script\nContent-Type: text/xml\nContent-length: $length\n"
-
-echo -e ${header}${msg}
-
+# Test ro contact SNAFU server
 nc -z $snafu $port
+
+# If contact is OK, send header with message.
 if [[ $? == 0 ]] ; then
   echo -e ${header}${msg} | nc -w 1 $snafu $port > :
   exit 0
