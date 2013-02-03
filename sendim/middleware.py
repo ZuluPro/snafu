d4c077547925e425d708e6a92626d0648c478798
Anthony MONTHE <montheanthony@hotmail.com>
2013-02-03 16:39:26 +0100

    Fix bug in middleware

--- sendim/middleware.py
+++ sendim/middleware.py
@@ -9,10 +9,11 @@ if 'djcelery' in settings.INSTALLED_APPS :
             if request.user.is_authenticated():
                 for task in TaskMeta.objects.filter(status="SUCCESS") :
                     task_result = task.result
-                    if task_result[1] == 'SUCCESS' :
-                        messages.add_message(request, messages.SUCCESS,'<b>Lecture de '+task_result[0]+u" termin\xe9e avec succ\xe8s !</b>")
-                    else :
-                        messages.add_message(request, messages.ERROR,'<b>Erreur de lecture de '+task_result[0]+' : '+str(task_result[2])+'</b>')
+                    if task_result : 
+                        if 'SUCCESS' in task_result :
+                            messages.add_message(request, messages.SUCCESS,'<b>Lecture de '+task_result[0]+u" termin\xe9e avec succ\xe8s !</b>")
+                        else :
+                            messages.add_message(request, messages.ERROR,'<b>Erreur de lecture de '+task_result[0]+' : '+str(task_result[2])+'</b>')
                     task.delete()
 
 class SnafuRequirementMiddleware(object):
